3e146a0f4facb7a046aa88b965ab32581cee1145
Deng Guangxing <dengguangxing@huawei.com>
2017-07-13 16:03:21 +0800

    bump containerd to cd60a4daf0da2ae6ca4ea84b9cbb2665fb4bdd4f
    Signed-off-by: Deng Guangxing <dengguangxing@huawei.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -126,7 +126,7 @@ clone git google.golang.org/api dc6d2353af16e2a2b0ff6986af051d473a4ed468 https:/
 clone git google.golang.org/cloud dae7e3d993bc3812a2185af60552bb6b847e52a0 https://code.googlesource.com/gocloud
 
 # containerd
-clone git github.com/docker/containerd 9c243484606eaac8e12d8d7362b72a332825fd93 git@code.huawei.com:docker/containerd.git
+clone git github.com/docker/containerd cd60a4daf0da2ae6ca4ea84b9cbb2665fb4bdd4f git@code.huawei.com:docker/containerd.git
 clone git github.com/tonistiigi/fifo 1405643975692217d6720f8b54aeee1bf2cd5cf4
 
 clean
